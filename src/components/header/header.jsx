4547f8c24367d91af56d4fe14ab2b5b3b8c28eb6
Stanislav Belov <mshmastermind@gmail.com>
2024-03-07 01:58:58 +0500

    c

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Button from "../button/button";
 import { useTelegram } from "../../hooks/useTelegram";
+import './header.css'
 
 export default function Header() {
 
